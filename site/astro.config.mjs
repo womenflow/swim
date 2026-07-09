45f7dd2488ec75eae842321d878cd14935d426b0
ahj1975423-lang <ahj1975423@gmail.com>
2026-07-09 18:06:17 +0900

    Remove site folder

--- site/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-// Update `site` to your deployed URL (Netlify address or custom domain).
-export default defineConfig({
-  site: 'https://bubblelime-sun-swim.netlify.app',
-  integrations: [sitemap()],
-});
